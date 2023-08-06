c17e899fe517d2b5447bb429b721c7f246a09694
Jiri Charamza <jicharamza@seznam.cz>
2023-08-06 19:11:54 +0200

    Project structure
    Add project structure

---
+++ src/main/java/net/jirkameister/xcom2mod/Xcom2Mod.java
@@ -0,0 +1,43 @@
+package net.jirkameister.xcom2mod;
+
+import com.mojang.logging.LogUtils;
+import net.jirkameister.xcom2mod.item.ModItems;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Xcom2Mod.MOD_ID)
+public class Xcom2Mod
+{
+    public static final String MOD_ID = "xcom2mod";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Xcom2Mod()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {}
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {}
+    }
+}
